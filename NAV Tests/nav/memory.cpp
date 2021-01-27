88aad58166ed1cfb01f47b3e72433ae8a15a6501
MurylloEx <muryllopimenta@gmail.com>
2021-01-27 20:41:43 -0300

    Partially fixed memory leak in NavGetFilesByProcessHandles
    Working to fix memory leak in NavGetProcessHandles.

--- NAV Tests/nav/memory.cpp
+++ NAV Tests/nav/memory.cpp
@@ -1,13 +1,23 @@
 #include "memory.h"
 
+#include <iostream>
+
 LPVOID NavAllocMem(SIZE_T SizeOfBlock) {
-	return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, SizeOfBlock);
+	void* addr = malloc(SizeOfBlock);
+	ZeroMemory(addr, SizeOfBlock);
+	return addr;
+	//return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, SizeOfBlock);
 }
 
 LPVOID NavReAllocMem(LPVOID BlockAddress, SIZE_T NewSize) {
-	return HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, BlockAddress, NewSize);
+	void* addr = realloc(BlockAddress, NewSize);
+	ZeroMemory(addr, NewSize);
+	return addr;
+	//return HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, BlockAddress, NewSize);
 }
 
 BOOL NavFreeMem(LPVOID BlockAddress) {
-	return HeapFree(GetProcessHeap(), NULL, BlockAddress);
+	free(BlockAddress);
+	return TRUE;
+	//return HeapFree(GetProcessHeap(), NULL, BlockAddress);
 }
