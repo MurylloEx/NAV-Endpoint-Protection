47f64dfd4c95d8fb933292484a465ebebc5e1a01
MurylloEx <muryllopimenta@gmail.com>
2021-01-27 22:21:52 -0300

    Partially fix of memory leak in NavGetProcessHandles
    Refactoring pending in handles.cpp functions

--- NAV Tests/nav/handles.h
+++ NAV Tests/nav/handles.h
@@ -1,114 +1,12 @@
 #pragma once
 
-#include <windows.h>
+#include "winapi.h"
+#include "memory.h"
 #include <tchar.h>
-
-#pragma comment(lib, "ntdll.lib")
-
-#define NT_SUCCESS(x) ((x) >= 0)
-#define STATUS_INFO_LENGTH_MISMATCH 0xc0000004
-
-#define SystemHandleInformation 16
-#define ObjectBasicInformation 0
-#define ObjectNameInformation 1
-#define ObjectTypeInformation 2
-
-typedef NTSTATUS(NTAPI *PNtQuerySystemInformation)(
-	ULONG SystemInformationClass,
-	PVOID SystemInformation,
-	ULONG SystemInformationLength,
-	PULONG ReturnLength
-	);
-
-typedef NTSTATUS(NTAPI *PNtDuplicateObject)(
-	HANDLE SourceProcessHandle,
-	HANDLE SourceHandle,
-	HANDLE TargetProcessHandle,
-	PHANDLE TargetHandle,
-	ACCESS_MASK DesiredAccess,
-	ULONG Attributes,
-	ULONG Options
-	);
-
-typedef NTSTATUS(NTAPI *PNtQueryObject)(
-	HANDLE ObjectHandle,
-	ULONG ObjectInformationClass,
-	PVOID ObjectInformation,
-	ULONG ObjectInformationLength,
-	PULONG ReturnLength
-	);
-
-typedef struct _UNICODE_STRING {
-	USHORT Length;
-	USHORT MaximumLength;
-	PWSTR Buffer;
-} UNICODE_STRING, *PUNICODE_STRING;
-
-typedef struct _SYSTEM_HANDLE {
-	ULONG ProcessId;
-	BYTE ObjectTypeNumber;
-	BYTE Flags;
-	USHORT Handle;
-	PVOID Object;
-	ACCESS_MASK GrantedAccess;
-} SYSTEM_HANDLE, *PSYSTEM_HANDLE;
-
-typedef struct _SYSTEM_HANDLE_INFORMATION {
-	ULONG HandleCount;
-	SYSTEM_HANDLE Handles[1];
-} SYSTEM_HANDLE_INFORMATION, *PSYSTEM_HANDLE_INFORMATION;
-
-typedef enum _POOL_TYPE {
-	NonPagedPool,
-	PagedPool,
-	NonPagedPoolMustSucceed,
-	DontUseThisType,
-	NonPagedPoolCacheAligned,
-	PagedPoolCacheAligned,
-	NonPagedPoolCacheAlignedMustS
-} POOL_TYPE, *PPOOL_TYPE;
-
-typedef struct _OBJECT_TYPE_INFORMATION {
-	UNICODE_STRING Name;
-	ULONG TotalNumberOfObjects;
-	ULONG TotalNumberOfHandles;
-	ULONG TotalPagedPoolUsage;
-	ULONG TotalNonPagedPoolUsage;
-	ULONG TotalNamePoolUsage;
-	ULONG TotalHandleTableUsage;
-	ULONG HighWaterNumberOfObjects;
-	ULONG HighWaterNumberOfHandles;
-	ULONG HighWaterPagedPoolUsage;
-	ULONG HighWaterNonPagedPoolUsage;
-	ULONG HighWaterNamePoolUsage;
-	ULONG HighWaterHandleTableUsage;
-	ULONG InvalidAttributes;
-	GENERIC_MAPPING GenericMapping;
-	ULONG ValidAccess;
-	BOOLEAN SecurityRequired;
-	BOOLEAN MaintainHandleCount;
-	USHORT MaintainTypeList;
-	POOL_TYPE PoolType;
-	ULONG PagedPoolUsage;
-	ULONG NonPagedPoolUsage;
-} OBJECT_TYPE_INFORMATION, *POBJECT_TYPE_INFORMATION;
-
-typedef struct _SYSTEM_PROCESS_INFO {
-	ULONG                   NextEntryOffset;
-	ULONG                   NumberOfThreads;
-	LARGE_INTEGER           Reserved[3];
-	LARGE_INTEGER           CreateTime;
-	LARGE_INTEGER           UserTime;
-	LARGE_INTEGER           KernelTime;
-	UNICODE_STRING          ImageName;
-	ULONG                   BasePriority;
-	HANDLE                  ProcessId;
-	HANDLE                  InheritedFromProcessId;
-} SYSTEM_PROCESS_INFO, *PSYSTEM_PROCESS_INFO;
 
 typedef struct _NAV_PROCESS_HANDLES {
 	POBJECT_TYPE_INFORMATION PObjectTypeInformation;
-	PSYSTEM_HANDLE Handle;
+	PHANDLE Handle;
 	HANDLE DuplicatedHandle;
 	LPVOID NextAddress;
 } NAV_PROCESS_HANDLES, *PNAV_PROCESS_HANDLES;
@@ -117,3 +15,22 @@ typedef struct _NAV_PROCESS_OPEN_FILES {
 	LPTSTR FilePathName;
 	LPVOID NextAddress;
 } NAV_PROCESS_OPEN_FILES, *PNAV_PROCESS_OPEN_FILES;
+
+
+/* NAV exported functions */
+LPTSTR	NavQueryFileNameByHandle	(
+	_In_ HANDLE FileHandle);
+
+BOOL	NavGetProcessHandles		(
+	_In_ ULONG ProcessId, 
+	_In_ PNAV_PROCESS_HANDLES lpNavProcessHandlesStruct);
+
+BOOL	NavFreeProcessHandles		(
+	_In_ PNAV_PROCESS_HANDLES lpNavProcessHandlesStruct);
+
+BOOL	NavGetFilesByProcessHandles(
+	_In_ PNAV_PROCESS_HANDLES lpNavProcessHandlesStruct,
+	_In_ PNAV_PROCESS_OPEN_FILES lpNavProcessOpenFilesStruct);
+
+BOOL	NavFreeOpenFiles(
+	_In_ PNAV_PROCESS_OPEN_FILES lpNavProcessOpenFilesStruct);
--- NAV Tests/nav/memory.cpp
+++ NAV Tests/nav/memory.cpp
@@ -3,21 +3,21 @@
 #include <iostream>
 
 LPVOID NavAllocMem(SIZE_T SizeOfBlock) {
-	void* addr = malloc(SizeOfBlock);
-	ZeroMemory(addr, SizeOfBlock);
-	return addr;
-	//return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, SizeOfBlock);
+	//void* addr = malloc(SizeOfBlock);
+	//ZeroMemory(addr, SizeOfBlock);
+	//return addr;
+	return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, SizeOfBlock);
 }
 
 LPVOID NavReAllocMem(LPVOID BlockAddress, SIZE_T NewSize) {
-	void* addr = realloc(BlockAddress, NewSize);
-	ZeroMemory(addr, NewSize);
-	return addr;
-	//return HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, BlockAddress, NewSize);
+	//void* addr = realloc(BlockAddress, NewSize);
+	//ZeroMemory(addr, NewSize);
+	//return addr;
+	return HeapReAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, BlockAddress, NewSize);
 }
 
 BOOL NavFreeMem(LPVOID BlockAddress) {
-	free(BlockAddress);
-	return TRUE;
-	//return HeapFree(GetProcessHeap(), NULL, BlockAddress);
+	//free(BlockAddress);
+	//return TRUE;
+	return HeapFree(GetProcessHeap(), NULL, BlockAddress);
 }
