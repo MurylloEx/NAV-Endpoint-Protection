47b1da9ac54724f77ca6bd23ba7f71b16fcc9bea
MurylloEx <muryllopimenta@gmail.com>
2021-01-31 15:57:34 -0300

    Finished the process protection by DACL API modification
    Beginning the implementation of file system protection by Discretionary Access Control Lists.

--- NAV Tests/nav/protection.h
+++ NAV Tests/nav/protection.h
@@ -6,8 +6,21 @@
 #include <accctrl.h>
 #include <AclAPI.h>
 
+typedef DWORD ACCESS_PERMISSIONS, *PACCESS_PERMISSIONS;
+
+NAVSTATUS NavCreateWellKnownSid(
+	IN WELL_KNOWN_SID_TYPE SidType,
+	IN PSID DomainSid,
+	IN PSID* SidPtr,
+	OUT DWORD* SidSizePtr);
+
+NAVSTATUS NavFreeWellKnownSid(
+	IN PSID* SidPtr);
+
 NAVSTATUS NavSetProcessKernelAce(
 	IN HANDLE ProcessHandle,
 	IN ACCESS_MODE AccessMode,
+	IN ACCESS_PERMISSIONS DesiredAccess,
 	IN TRUSTEE_FORM TrusteeFormat,
-	IN LPWSTR TrusteeName);
+	IN TRUSTEE_TYPE TrusteeType,
+	IN LPVOID TrusteeName);
